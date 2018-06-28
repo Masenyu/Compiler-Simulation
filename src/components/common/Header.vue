8a07ecd56813a0a87e049c0493028d2c8d376dae
RenJNing <835380624@qq.com>
2018-06-28 14:18:24 +0800

    修改了页面上方Header的样式

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,21 +1,21 @@
 <template>
     <el-row>
         <div class="header">
-            <div class="logo_nav">
-                <div class="logo">
-                    <span>Compiler Stimulation</span>
-                </div>
-                <div class="nav-menu">
-                    <el-menu :default-active="activeIndex" text-color="#040404" class="el-menu-demo" mode="horizontal" id="navigator" unique-opened router>
-                        <template v-for="item in items">
-                            <el-menu-item :index="item.index">
-                            <template class="nav-tab"><i :class="item.icon"></i><span class="nav-tab">{{ item.title }}</span></template>
-                            </el-menu-item>
-                        </template>
-                    </el-menu>
-                </div>
+          <div class="navigation">
+            <div class="logo">
+              <img style="float:left" src="../../assets/_logo.png" width="40px" height="40px"/>
+              <a style="float:left">文法分析模拟器</a>
             </div>
-            <div class="user-info">
+            <div class="nav-menu">
+              <ul>
+                <li><a @click="gotoUrl('/index/main-interface')">首页</a></li>
+                <li><a @click="gotoUrl('/index/lexical-analysis')">词法分析</a></li>
+                <li><a @click="gotoUrl('/index/main-interface')">语法分析</a></li>
+                <li><a @click="gotoUrl('/index/main-interface')">语义分析</a></li>
+                <li><a class='login-btn'>登录</a></li>
+              </ul>
+            </div>
+            <!-- <div class="user-info">
                 <el-button style="display:none;">登陆</el-button>
                 <el-dropdown>
                     <span class="el-dropdown-link">
@@ -27,7 +27,8 @@
                         <el-dropdown-item divided>注销</el-dropdown-item>
                     </el-dropdown-menu>
                 </el-dropdown>
-            </div>
+            </div> -->
+          </div>
         </div>
     </el-row>
 </template>
@@ -36,30 +37,12 @@
 export default {
   data () {
     return {
-      name: '账号',
-      activeIndex: '/index/lexical-analysis',
-      items: [
-        {
-          icon: 'el-icon-menu',
-          index: '/index/main-interface',
-          title: '首页'
-        },
-        {
-          icon: 'el-icon-menu',
-          index: '/index/lexical-analysis',
-          title: '词法分析'
-        },
-        {
-          icon: 'el-icon-menu',
-          index: '/index/gramma-analysis',
-          title: '语法分析'
-        },
-        {
-          icon: 'el-icon-menu',
-          index: '/index/semantic-analysis',
-          title: '语义分析'
-        }
-      ]
+      name: '账号'
+    }
+  },
+  methods: {
+    gotoUrl (url) {
+      this.$router.push(url)
     }
   }
 }
@@ -67,48 +50,72 @@ export default {
 
 <style scoped>
 .header {
-  position: relative;
   width: 100%;
-  height: 60px;
+  height: 40px;
+  color: #49505b;
+  font-size: 16px;
+  font-weight: 400;
+  /* line-height: 1.5em; */
+  padding:15px 0px;
+  box-shadow: 0px 1px 1px #ece9e9;
+}
+.navigation{
+  position: relative;
   overflow: hidden;
   white-space: nowrap;
-  min-width: 1200px;
-  background-color: #eeeeee;
-  line-height: 60px;
-}
-.logo_nav {
-  display: inline-block;
-  height: 60px;
-  line-height: 60px;
-}
-.nav-menu {
-  text-align: center;
-  width: 610px;
-  height: 60px;
-  display: inline-block;
+  min-width: 800px;
+  margin:0px auto;
+  width:65%;
 }
 .logo {
-  text-align: center;
-  width: 350px;
-  height: 60px;
-  font-size: 28px;
-  /* display:inline-block; */
-  float: left;
-  margin-top: 0px;
-}
-.user-info {
-  width: 80px;
-  height: 60px;
-  display: inline-block;
-  position: absolute;
-  right: 10px;
+  font-size: 32px;
   cursor: pointer;
+  -webkit-text-size-adjust:100%;
+  min-width: 270px;
+  float: left;
 }
-.el-menu-demo {
-  background-color: #eeeeee;
+.nav-menu {
+  height: 40px;
+  float: right;
 }
-.nav-tab {
-  font-size: 16px;
-  height: 60px;
+ul{
+  list-style:none; /* 去掉ul前面的符号 */
+  margin: 0px; /* 与外界元素的距离为0 */
+  padding: 0px; /* 与内部元素的距离为0 */
+  width: auto; /* 宽度根据元素内容调整 */
+}
+ul li
+{
+  padding:0px 10px;
+  float:left; /* 向左漂移，将竖排变为横排 */
+}
+ul li a, ul li a:visited
+{
+  cursor: pointer;
+  color:#49505b;
+  display: block; /* 此元素将显示为块级元素，此元素前后会带有换行符 */
+  padding: 10px; /* 内部填充的距离 */
+  text-decoration: none; /* 不显示超链接下划线 */
+  white-space: nowrap; /* 对于文本内的空白处，不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止。 */
+}
+ul li a:hover
+{
+  color: #39a9db; /* 文字颜色 */
+  text-decoration: none; /* 不显示超链接下划线 */
+}
+ul li a:active
+{
+  color:#49505b;/* 文字颜色 */
+  text-decoration: none; /* 不显示超链接下划线 */
+}
+.login-btn{
+  cursor: pointer;
+  border: 1px solid #00aeee;
+  color: #00aeee;
+  border-radius: 3px;
+  display: block; /* 此元素将显示为块级元素，此元素前后会带有换行符 */
+  padding: 8px 20px; /* 内部填充的距离 */
+  text-decoration: none; /* 不显示超链接下划线 */
+  white-space: nowrap; /* 对于文本内的空白处，不会换行，文本会在在同一行上继续，直到遇到 <br> 标签为止。 */
 }
 </style>
