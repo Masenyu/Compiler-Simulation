4c6da749395c629f6ada9f8f580deffaeb0f01ce
zhoudongdong <2210663134@qq.com>
2018-06-28 17:06:57 +0800

    add generateNFA.test.js and generateDFA.test.js

--- test/test.py
+++ test/test.py
@@ -1,18 +1,10 @@
 from selenium import webdriver
 import time
-
-driver = webdriver.Firefox()
-f_start_time = time.time();
-driver.get("http://localhost:8088/#/index/lexical-analysis")
-print("Firefox: ",end="")
-
-
-button_start_participle= driver.find_element_by_css_selector(".el-button.el-button--primary")
-button_start_participle.click()
-
-button_auto_show= driver.find_element_by_css_selector(".el-button.el-button--primary.is-plain")
-button_auto_show.click()
-
-f_end_time = time.time()
-Firefoxtime = f_end_time - f_start_time
-print(str(Firefoxtime) + "ms")
+import os
+driver = webdriver.Edge()
+driver.get("http://www.baidu.com")
+print(driver.title)
+driver.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("su").click()
+time.sleep(3)
+driver.close()
