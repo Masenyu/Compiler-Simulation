b3014c8dd0ee515b7c1f0307a563c4e10e83f373
ziwei <2369969039@qq.com>
2018-07-02 17:27:12 +0800

    初步实现DFA布局算法

--- src/api/vis_api.js
+++ src/api/vis_api.js
@@ -1,5 +1,6 @@
 export { createEdges, createNodes }
 import { NFALayout } from './nfaLayout'
+import { DFALayout } from './dfaLayout'
 function createEdges(transitionTable, alphabet, openPlug = true) {
   var Edges = []
   let _range = length => Array.from({ length }).map((v, k) => k)
@@ -7,17 +8,19 @@ function createEdges(transitionTable, alphabet, openPlug = true) {
   let transitable_hack = deepcopy(transitionTable);
   if (alphabet.indexOf('ε') != -1) {
     if (openPlug === true) transitable_hack = NFALayout(transitable_hack, alphabet)[0];
+  } else {
+    if (openPlug === true) transitable_hack = DFALayout(transitable_hack, alphabet)[0];
   }
 
   for (var fState of _range(transitionTable.length)) {
     for (var chIndex of _range(alphabet.length)) {
       for (var tState of transitionTable[fState][chIndex]) {
         if (transitable_hack[fState][chIndex].indexOf(tState) != -1) {
-          if (alphabet.indexOf('ε') != -1) console.log(`edge: from ${fState} to ${tState}, in transitable_hack`);
-          Edges.push({ id: Edges.length, from: fState, to: tState, arrows: 'to', label: alphabet[chIndex]})
+          //if (alphabet.indexOf('ε') != -1) console.log(`edge: from ${fState} to ${tState}, in transitable_hack`);
+          Edges.push({ id: Edges.length, from: fState, to: tState, arrows: 'to', label: alphabet[chIndex] })
         } else {
           if (alphabet.indexOf('ε') != -1) console.log(`edge: from ${fState} to ${tState}, not in transitable_hack`);
-          Edges.push({ id: Edges.length, from: fState, to: tState, arrows: 'to', label: alphabet[chIndex], smooth:{type:'curvedCW',roundness:0.5} })
+          Edges.push({ id: Edges.length, from: fState, to: tState, arrows: 'to', label: alphabet[chIndex], smooth: { type: 'curvedCW', roundness: 0.5 } })
         }
       }
     }
@@ -42,6 +45,13 @@ function createNodes(transitionTable, acceptState, alphabet) {
   if (alphabet.indexOf('ε') != -1) {
     let nodesLevelInfo = NFALayout(deepcopy(transitionTable), alphabet)[1];
     nodesLevelInfo.forEach((level, node) => {
+      //console.log(`node: ${node}, level: ${level}`)
+      Nodes[node]['level'] = level;
+    })
+    Nodes[Nodes.length - 1]['level'] = 0;
+  } else {
+    let nodesLevelInfo = DFALayout(deepcopy(transitionTable), alphabet)[1];
+    nodesLevelInfo.forEach((level, node) => {
       console.log(`node: ${node}, level: ${level}`)
       Nodes[node]['level'] = level;
     })
@@ -52,6 +62,9 @@ function createNodes(transitionTable, acceptState, alphabet) {
   return Nodes
 }
 function deepcopy(arr) {
+  /**
+   * @description 实现多维数组的深复制
+   */
   var out = [], i = 0, len = arr.length;
   for (; i < len; i++) {
     if (arr[i] instanceof Array) {
